5ce6edf6b87f17105e4009087a03c9552b56a1b7
KelseyDang2002 <kelseydang813@gmail.com>
2024-08-09 13:37:56 -0700

    update  2

--- stuff/dataset.py
+++ stuff/dataset.py
@@ -1,7 +1,27 @@
 import csv
 
-file = r"C:\Users\Kelsey PC\Downloads\cars.csv"
+DIMENSIONS_HEIGHT = 0
+DIMENSIONS_LENGTH = 1
+DIMENSIONS_WIDTH = 2
+ENGINE_INFORMATION_DRIVELINE = 3
+ENGINE_INFORMATION_ENGINETYPE = 4
+ENGINE_INFORMATION_HYBRID = 5
+ENGINE_INFORMATION_GEARS = 6
+ENGINE_INFORMATION_TRANSMISSION = 7
+FUEL_INFORMATION_CITYMPG = 8
+FUEL_INFORMATION_FUELTYPE = 9
+FUEL_INFORMATION_HIGHWAYMPG = 10
+IDENTIFICATION_CLASSIFICATION = 11
+IDENTIFICATION_ID = 12
+IDENTIFICATION_MAKE = 13
+IDENTIFICATION_MODELYEAR = 14
+IDENTIFICATION_YEAR = 15
+ENGINE_STATISTICS_HORSEPOWER = 16
+ENGINE_STATISTICS_TORQUE = 17
 
+INDEX_START = 1
+
+file = r"C:\Users\Kelsey PC\Downloads\cars.csv"
 
 def program_exit(message):
     print(f"\n\n\n{message} -> Program exiting...")
@@ -9,29 +29,30 @@ def program_exit(message):
     exit(0)
 
 def user_input():
-    rowNumber = int(input("\nEnter number of rows: "))
-    print("")
+    rowNumber = int(input("Enter desired number of rows: "))
     return rowNumber
 
 def dataset_read():
-    rows = []
+    rowList = []
     with open(file) as csvFile:
         csvReader = csv.reader(csvFile, delimiter=",")
-
         for row in csvReader:
-            rows.append(row)
+            rowList.append(row)
+        print(f"Total no. of rows: {csvReader.line_num}")
 
-        print("Total no. of rows: %d" % (csvReader.line_num))
+    print(f"Total no. of Columns: {len(rowList[0])}\n")
+    print(f"Fields: {rowList[0]}\n")
+    return rowList
 
-    print("Columns:\n", rows[0])
+def menu():
+    print("")
 
-    return rows
-
-def get_rows(rows, n):
-    for row in rows[1:n + 1]:
-        print(row, "\n")
+def display_rows(rowList, rowNumber):
+    for index, row in enumerate(rowList[1:rowNumber + 1], INDEX_START):
+        print(f"\n{index}. {row[IDENTIFICATION_ID]}:")
+        print(f"{row}")
 
 if __name__ == "__main__":
-    rows = dataset_read()
+    rowList = dataset_read()
     rowNumber = user_input()
-    get_rows(rows, rowNumber)
+    display_rows(rowList, rowNumber)
