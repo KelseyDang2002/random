7cba4518504f0ae51b314589b8a24ced911eb7a0
KelseyDang2002 <kelseydang813@gmail.com>
2024-08-21 15:29:14 -0700

    update 4

--- cppbook/ch3.cpp
+++ cppbook/ch3.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <ctime>
+
 using std::cout;
+using std::cin;
 using std::endl;
 using std::string;
+using std::time;
 
 void counter();
 void string_example();
 void inventory();
 void tic_tac_toe();
+void word_jumble();
 
 int main()
 {
     // counter();
     // string_example();
     // inventory();
-    tic_tac_toe();
+    // tic_tac_toe();
+    word_jumble();
     return 0;
 }
 
@@ -153,7 +160,8 @@ void tic_tac_toe()
 {
     const int ROWS = 3;
     const int COLUMNS = 3;
-    char board[ROWS][COLUMNS] = {
+    char board[ROWS][COLUMNS] =
+    {
         {'O', 'X', 'O'},
         {' ', 'X', 'X'},
         {'X', 'O', 'O'}
@@ -185,4 +193,66 @@ void tic_tac_toe()
     }
 
     cout << "\n'X' wins!";
+}
+
+void word_jumble()
+{
+    enum fields {WORD, HINT, NUM_FIELDS};
+    const int NUM_WORDS = 5;
+    const string WORDS[NUM_WORDS][NUM_FIELDS] =
+    {
+        {"wall", "Do you feel like banging your head against something?"},
+        {"glasses", "These might help you see the answer."},
+        {"labored", "Going slowly, is it?"},
+        {"persistent", "Keep at it."},
+        {"jumble", "It's what the ganme is all about."}
+    };
+
+    srand(static_cast<unsigned int>(time(0)));
+    int choice = (rand() % NUM_WORDS);
+    string theWord = WORDS[choice][WORD]; // word to guess
+    string theHint = WORDS[choice][HINT]; // hint for word
+
+    string jumble = theWord; // jumbled version of word
+    int length = jumble.size();
+    for (int i = 0; i < length; i++)
+    {
+        int index1 = (rand() % length);
+        int index2 = (rand() % length);
+        char temp = jumble[index1];
+        jumble[index1] = jumble[index2];
+        jumble[index2] = temp; 
+    }
+
+    cout << "\t\tWelcome to Word Jumble!\n\n";
+    cout << "Unscramble the letters to make a word.\n";
+    cout << "Enter 'hint' for a hint.\n";
+    cout << "Enter 'quit' to tquit the game.\n\n";
+    cout << "The jumble is: " << jumble;
+
+    string guess;
+    cout << "\n\nyour guess: ";
+    cin >> guess;
+
+    while ((guess != theWord) && (guess != "quit"))
+    {
+        if (guess == "hint")
+        {
+            cout << theHint;
+        }
+        else
+        {
+            cout << "Sorry, that's not it.";
+        }
+
+        cout << "\n\nYour guess: ";
+        cin >> guess;
+    }
+
+    if (guess == theWord)
+    {
+        cout << "\nThat's it! You guessed it!\n";
+    }
+
+    cout << "\nThanks for playing.\n";
 }
---
+++ cppbook/ch4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void inventory_2();
+
+int main()
+{
+    inventory_2();
+    return 0;
+}
+
+void inventory_2()
+{
+    cout << "test\n";
+}
